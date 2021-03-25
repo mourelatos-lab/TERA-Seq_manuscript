c630ad6d3d555770cdfb0c14e24504fc29c0c17e
opplatek <opplatek@gmail.com>
2021-03-25 17:32:08 -0400

    1732/03252021

--- git-commit.sh
+++
@@ -1,8 +0,0 @@
-#git init
-git add .
-#git commit -m "first commit"
-VERSION=$(date +'%H%M/%m%d%Y')
-git commit -m "$VERSION"
-#git branch -M main
-#git remote add origin https://github.com/mourelatos-lab/TERA-Seq_manuscript.git
-git push -u origin main
